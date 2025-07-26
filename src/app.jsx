b04b999f9e4c8b0963e49b75343a112d497403ed
JAGA <ramujagan2004@gmail.com>
2025-07-26 22:08:07 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
+import "./styles/main.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
 
